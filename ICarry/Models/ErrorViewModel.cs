e9802b69613725e4d43f4fd1a95173105030db8f
Jfgg1991 <jfgg.trabajos@gmail.com>
2019-09-30 19:26:11 +0200

    FistCommit
    Archivo base, es para comenzar a tirar lo que se necesite de los casos de uso, no teiene modelos

---
+++ ICarry/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ICarry.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
